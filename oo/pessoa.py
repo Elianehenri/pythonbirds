721d1693444520d2665b89c7748f9f871a9e15f1
elianehenri <elianehenriqueta@gmail.com>
2021-09-27 14:33:09 -0300

    Criada a classe Pessoa

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,2 +1,12 @@
 class Pessoa:
-    pass
+    def cumprimentar(self):
+        return f'Ola {id(self)}'
+
+
+    if __name__ == '__main__':
+        P = Pessoa()
+        print(Pessoa.cumprimentar(p))
+        print(id(p))
+        print(p.cumprimentar())
+
+
